9835770abbf296ada0bf3c33a53ec092978a4bd7
Dmytro.Kovalov <kovalov.d.a@gmail.com>
2018-04-19 18:47:09 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,38 @@
+﻿#include <stdint.h> 
+
+class Vehicle
+{
+private:
+    uint32_t speed;
+};
+
+class Character
+{
+public:
+    virtual void increaseMaxEnergy ( uint32_t ) = 0;
+    virtual void decreaseEnergy ( uint32_t ) = 0;
+private:
+    int32_t cash;
+    uint32_t happiness;
+    uint32_t energy;
+    uint32_t maxEnergy;
+    Vehicle *vehicle;
+};
+
+class GraphContent
+{
+    void interface(void);
+private:
+};
+
+class GraphAlgorithm
+{
+public:
+};
+
+
+
+int main ( )
+{
+    return 0;
+}
