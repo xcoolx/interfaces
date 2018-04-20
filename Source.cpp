eb205a33c631669e96eeb2464b10d9ada1d1a51f
Dmytro.Kovalov <kovalov.d.a@gmail.com>
2018-04-20 18:47:54 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,34 +1,107 @@
-﻿#include <stdint.h> 
+﻿#include <stdint.h>
+#include <string>
+#include <vector>
 
-class Vehicle
-{
-private:
-    uint32_t speed;
-};
-
-class Character
+class Outputter
 {
 public:
-    virtual void increaseMaxEnergy ( uint32_t ) = 0;
-    virtual void decreaseEnergy ( uint32_t ) = 0;
-private:
-    int32_t cash;
-    uint32_t happiness;
-    uint32_t energy;
-    uint32_t maxEnergy;
-    Vehicle *vehicle;
+    virtual void output ( std::string ) = 0;
 };
 
-class GraphContent
+class Logger
 {
-    void interface(void);
 private:
-};
-
-class GraphAlgorithm
-{
+    static Outputter* outputter;
+    static std::vector<std::string> storage;
+    static Logger* logger;
+    Logger ( );
+    ~Logger ( );
 public:
+    static Logger* getLogger ( );
+    static void addRecord ( std::string record );
+    static void output ( );
 };
+
+void Logger::addRecord ( std::string record )
+{
+    storage.push_back ( record );
+}
+
+Logger * Logger::logger = nullptr;
+
+void Logger::output ( )
+{
+    std::vector<std::string>::iterator it;
+    for (it = storage.begin ( ); it != storage.end ( ); it++)
+    {
+        outputter->output( *it );
+    }
+}
+
+Logger* Logger::getLogger ( )
+{
+    if (nullptr == logger)
+    {
+        logger = new Logger;
+    }
+    return logger;
+}
+
+Logger::~Logger ( )
+{
+    if (nullptr != logger)
+    {
+        delete logger;
+    }
+}
+
+namespace characterLimitation
+{
+    const uint32_t MAX_ABILITY_VALUE = 1000;
+};
+
+class Ability
+{
+private:
+    uint32_t currentValue;
+    const uint32_t limit;
+public:
+    explicit Ability ( uint32_t, const uint32_t limit );
+    Ability ( ) = delete;
+    void improve ( uint32_t );
+    uint32_t getValue ( );
+};
+
+class Capability
+{
+private:
+
+};
+
+Ability::Ability ( uint32_t value, const uint32_t _limit = characterLimitation::MAX_ABILITY_VALUE ) : currentValue ( value ), limit ( _limit )
+{
+    if (currentValue > limit/2)
+    {
+        Logger *log = Logger::getLogger ( );
+        log->addRecord ( " wrong initialization " );
+
+        currentValue = limit;
+    }
+};
+
+void Ability::improve ( uint32_t value )
+{
+    currentValue += value;
+    if (limit < currentValue)
+    {
+        currentValue = limit;
+    }
+}
+
+uint32_t Ability::getValue ( )
+{
+    return currentValue;
+}
 
 
 
